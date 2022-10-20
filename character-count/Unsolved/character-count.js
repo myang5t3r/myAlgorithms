465976fecf6b82d3890d5e5c55d6836454b6b13e
myang5t3r <matthew.yang@hunterdouglas.com>
2022-10-20 09:56:41 -0600

    Completed charcter-count algorithm

--- character-count/Unsolved/character-count.js
+++ character-count/Unsolved/character-count.js
@@ -1,3 +1,19 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function (str) {
+  //   Split string into array
+  // create object of each letter of the split string
+  //   count the letters
+  const myObject = {};
+  splitArray = str.split("");
+  for (const element of splitArray) {
+    // count number of time element is in the string
+    let count = str.split(element).length - 1;
+    myObject[element] = count;
+  }
+  return myObject;
+};
+
+const myString = "Matt loves to code";
+
+console.log(characterCount(myString));
