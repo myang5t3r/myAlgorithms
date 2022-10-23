85a9f1b016a2c9ff7f7ceb49b372c900dae5f03b
myang5t3r <matthew.yang@hunterdouglas.com>
2022-10-23 13:01:50 -0600

    Completed double-trip-map algorithm

--- double-triple-map -DONE/Unsolved/double-triple-map.js
+++ double-triple-map -DONE/Unsolved/double-triple-map.js
@@ -2,4 +2,18 @@
 // If a element in the original array is even, the element at the same index in the new array should be double the original element
 // If an element in the original array is odd, the element at the same index of the new array should be triple the original element
 
-var doubleTripleMap = function(arr) {};
+var doubleTripleMap = function (arr) {
+  const resultArr = [];
+  for (const element of arr) {
+    if (element % 2 === 0) {
+      resultArr.push(element * 2);
+    } else if (element % 2 !== 0) {
+      resultArr.push(element * 3);
+    }
+  }
+  return resultArr;
+};
+
+const myArr = [1, 2, 3, 4];
+
+doubleTripleMap(myArr);
