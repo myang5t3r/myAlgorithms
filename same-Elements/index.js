17b76a8728ee97ed7182ec4877db663ac6c9b58c
myang5t3r <matthew.yang@hunterdouglas.com>
2022-11-30 08:24:49 -0700

    Same element algo

--- same-Elements/index.js
+++ same-Elements/index.js
@@ -23,10 +23,31 @@ function sameElement(arr) {
         returnVar = element;
       }
     });
+    arr.push(firstElement);
   }
   return returnVar;
 }
 
+function solution2(arr) {
+  // create return variable
+  let returnVal = -1;
+  // find half of length of array
+  const half = Math.floor(arr.length / 2);
+  // console.log(half);
+  // Loop through each element of array and use array method filter
+  arr.forEach((element) => {
+    const newArr = arr.filter((index) => index === element);
+    // console.log(newArr);
+    if (newArr.length >= half) {
+      returnVal = element;
+    }
+  });
+  return returnVal;
+}
+
 const myArr = [1, 2, 3, 4, 4, 4, 4];
 const myArr2 = [3, 5, 5, 5, 5, 6, 6, 6];
-console.log(sameElement(myArr2));
+const myArr3 = [5, 6, 10, 4, 66, 11];
+
+console.log(sameElement(myArr3));
+console.log(solution2(myArr2));
