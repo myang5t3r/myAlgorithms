b670bff5d001fc9ad1eea35a71dd159142d177e0
myang5t3r <matthew.yang@hunterdouglas.com>
2022-12-02 13:31:15 -0700

    min-increment algo completed

--- 02-min-increment-for-unique/Unsolved/min-increment-for-unique.js
+++ 02-min-increment-for-unique/Unsolved/min-increment-for-unique.js
@@ -2,23 +2,34 @@
 
 const minIncrement = function (nums) {
   // return val
-  const val = 0;
-  //  if array length = 0 return 0
-  if (nums.length === 0) {
-    return val;
-  }
-  // if array is unique return 0
-  nums.forEach((element) => {
-    const newArr = nums.filter((index) => index === element);
-    if (newArr.length != 1) {
-      return;
+  let val = 0;
+  //   //  if array length = 0 return 0
+  //   if (nums.length === 0) {
+  //     return val;
+  //   }
+  //   // if array is unique return 0
+  //   nums.forEach((element) => {
+  //     const newArr = nums.filter((index) => index === element);
+  //     if (newArr.length != 1) {
+  //       return;
+  //     }
+  //   });
+  const newArr = nums.sort((a, b) => a - b);
+  console.log(newArr);
+  for (let i = 1; i < newArr.length; i++) {
+    console.log(newArr[i]);
+    if (newArr[i] <= newArr[i - 1]) {
+      const temp = newArr[i];
+      console.log(temp);
+      newArr[i] = newArr[i - 1] + 1;
+      console.log(newArr[i]);
+      val += newArr[i] - temp;
+      console.log(val);
     }
-  });
-
-  //   const newArr = nums.sort((a, b) => a - b);
-  //   console.log(newArr);
+  }
+  console.log(val);
   return val;
 };
 
-const myArr = [2, 8, 5, 6, 4, 1, 100];
+const myArr = [0, 0, 1, 2, 2, 3, 0];
 minIncrement(myArr);
